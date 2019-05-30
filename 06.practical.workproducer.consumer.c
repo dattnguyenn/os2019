aa7fe0f222d0fd54e70af85b095917a07110e5e1
mac <mac@MacBook-cua-mac.local>
2019-05-30 15:10:48 +0700

    fix labwork 6

--- 06.practical.workproducer.consumer.c
+++ 06.practical.workproducer.consumer.c
@@ -16,48 +16,43 @@ item buffer[BUFFER_SIZE];
 int first = 0;
 int last = 0;
 
-void printLog(){
-  printf("first = %d\t last = %d\n", first, last);
+void Log(){
+  printf("the first item is= %d\t the last item is = %d\n", first, last);
   return;
 }
 
 void produce(item *i){
   while ((first + 1) % BUFFER_SIZE == last){
-    //do nothing -- no free buffer item
-    printf("No free buffer item!\n");
   }
   memcpy(&buffer[first], i, sizeof(item));
   first = (first + 1) % BUFFER_SIZE;
-  printLog();
+  Log();
 }
 
 item* consume(){
   item* i = malloc(sizeof(item));
   while (first == last){
-    // do nothing -- nothing to consume
-    printf("Nothing to consume!\n");
+  
   }
   memcpy(i, &buffer[last], sizeof(item));
   last = (last + 1) % BUFFER_SIZE;
-  printLog();
+  Log();
   return i;
 }
 
-item* initItem(char type, int amount, char unit){
-  item* i = malloc(sizeof(item));
-  i->type = type;
-  i->amount = amount;
-  i->unit = unit;
-  return i;
-}
-
-int main(){
-  item* order1 = initItem('1',3,'0');
-  item* order2 = initItem('0',9,'1');
-  printf("Initial log:\nfirst = %d\tlast = %d\n\n", first, last);
-  produce(order1);
-  produce(order2);
-  consume();
+int main()
+{ item cake;
+    cake.type = 'c';
+    cake.amount = 5;
+    cake.unit = 'g';
+    item candy;
+    candy.type = 's';
+    candy.amount = 8;
+    candy.unit = 'g';
+       produce(&cake);
+       produce(&candy);
+    item*pointer= consume();
+    free(pointer);
   return 0;
 }
 
